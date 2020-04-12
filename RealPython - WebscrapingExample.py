e95e758ae8ec9872894442deec538d12d01cd426
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-04-12 21:53:16 +0200

    More

--- RealPython - WebscrapingExample.py
+++ RealPython - WebscrapingExample.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # https://realpython.com/beautiful-soup-web-scraper-python/
 
 import requests
@@ -13,23 +14,17 @@ job_elems = results.find_all("section", class_="card-content")
             #Einzelne Elemente des Suchergebnisses links werden eingelesen
             #und in einem Array gespeichert
 for job_elem in job_elems:
-        #für jedes einzelne Job-Suchergebnis wird der Teil für Titel / Company / Location gespeichert
+        # fÃ¼r jedes einzelne Job-Suchergebnis wird der Teil fÃ¼r Titel - Company - Location gespeichert
     title_elem = job_elem.find("h2", class_="title")
     company_elem = job_elem.find("div", class_="company")
     location_elem = job_elem.find("div", class_="location")
     if None in (title_elem, company_elem, location_elem):   # Wenn Absturz wg. leerem Element - z.B. wg. Foto ohne Text
-                                                            # erfolgt keine Weiterverarbeitung
-        continue
-    print(title_elem.text.strip())      # Textteil wird herauskopiert für den jeweiligen Bereich
-    print(company_elem.text.strip())    # für Titel / Company / Location
+        continue                                            # erfolgt keine Weiterverarbeitung
+    print(title_elem.text.strip())      # Textteil wird herauskopiert fÃ¼r den jeweiligen Bereich
+    print(company_elem.text.strip())
     print(location_elem.text.strip(), "\n")
 
-python_jobs = results.find_all("h2", string=lambda text: "area" in text.lower())
-    # hier ein  Beispiel wenn nur bestimmte Jobs mit einem bestimmten Text ausgegeben werden sollen
-
-
-
-
-
-
+python_jobs = results.find_all("h2",
+                               string=lambda text: "area" in text.lower())
+print (len(python_jobs))
 
