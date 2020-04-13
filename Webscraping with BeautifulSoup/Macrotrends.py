32d0229aa7fe88a8cddb6c7693f187cc01ed5be5
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-04-13 16:17:15 +0200

    More

--- Webscraping with BeautifulSoup/Macrotrends.py
+++ Webscraping with BeautifulSoup/Macrotrends.py
@@ -1,6 +1,7 @@
 # https://www.macrotrends.net/stocks/charts/AAPL/apple/shares-outstanding
 
 import requests
+import datetime
 import re
 from bs4 import BeautifulSoup
 
@@ -11,10 +12,22 @@ soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="style-1")
 # print(results.prettify())
 
-entries = results.find_all("td")
-# print(entries)
-for i, entry in enumerate(entries):
+entries = results.find_all("div", class_="col-xs-6")
+entries.pop(0)
+print(entries)
+
+"""
+    entries_final = []
+    for i, entry in enumerate(entries):
+        if i%2 != 0:
+            continue
+        else:
+            datetime.datetime.strptime(entry, "%y/%m/%d")
     print(i, entry.text)
+"""
+
+
+
 
 
 
