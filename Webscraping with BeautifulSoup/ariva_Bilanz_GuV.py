1b3ea2de2417a5c5e5f8af03d4b2350f1dc20152
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-04-15 22:11:53 +0200

    More

---
+++ Webscraping with BeautifulSoup/ariva_Bilanz_GuV.py
@@ -0,0 +1,33 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+# Ausgabe der Liste als CSV-File inkl. Prüfung ob Datei geöffnet ist
+# Input content: ist eine Matrix Liste [[][]]
+# Input filenmae: Name des CSVs-File
+def csv_write(content,filename):
+    while True:
+        try:
+            with open (filename,"w",newline="") as fp:
+                a = csv.writer(fp,delimiter=",")
+                a.writerows(content)
+                break
+        except:
+            input ("Datei kann nicht geöffent werden - bitte schließen und <Enter> drücken!")
+
+page = requests.get ("https://www.ariva.de/apple-aktie/bilanz-guv#stammdaten")
+soup = BeautifulSoup (page.content, "html.parser")
+table = soup.find_all ("div", class_="column twothirds table")
+output = []
+for i in table:
+    for j in i.find_all("tr"):
+        print (j.prettify ())
+        row = []
+        for k in j.find_all("td"):
+            row.append(k.text.strip())
+        output.append(row)
+#print(output)
+#csv_write(output,"ariva_data.csv")
+
+
+
