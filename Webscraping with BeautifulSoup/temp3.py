446d8d4f31b99044691103c87327f7c67197b628
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-04-18 16:42:51 +0200

    More

--- Webscraping with BeautifulSoup/temp3.py
+++ Webscraping with BeautifulSoup/temp3.py
@@ -1,37 +1,11 @@
-import  subprocess, re, random, datetime
+import subprocess, re, random, datetime
+from random import randrange
 
-"""
-def statusCheck():   
-    # This function will run a status check on your nordvpn service to see if you are already connected.  You must be disconnected for this program to work.        
-    nord_output = subprocess.Popen (["nordvpn", "status"], stdout=subprocess.PIPE)
-    status = re.split ("[\r \n :]", nord_output.communicate ()[0].decode ("utf-8"))[-2]
-
-    if status == "Disconnected":
-        return True
-
-    else:
-        disconnect = input (
-            "You are currently connected to NordVPN already.  Would you like to disconnect and continue? [y/n]: ").lower ()
-
-        if disconnect == "y":
-            subprocess.call (["nordvpn", "disconnect"])
-            return True
-
-        elif disconnect == "n":
-            print ("You have chosen not to disconnect from your current NordVPN session.  Exiting program...")
-            exit ()
-
-def getCountries(): 
-    # This function will return a list of the current countries with available servers for your nordvpn account.
-    nord_output = subprocess.Popen(["nordvpn", "countries"], stdout=subprocess.PIPE)
-    countries = re.split("[\t \n]", nord_output.communicate()[0].decode("utf-8"))
-
-    while "" in countries:
-        countries.remove("")
-
-    return countries
-
-laender = getCountries()
-"""
-
-nord_output = subprocess.Popen(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "countries"], stdout=subprocess.PIPE)
+# nord_output = subprocess.Popen(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "status"], stdout=subprocess.PIPE)
+#subprocess.call(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "-c -g Belgium"])
+#subprocess.call(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "-c"])
+#subprocess.call(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "-d"])
+#subprocess.call(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "-c", "-g Austria"])
+countries = ["Albania", "Argentina", "Australia", "Austria", "Belgium", "Canada", "Germany", "Israel", "Italy", "Norway",
+             "Poland", "Portugal", "Romania", "Serbia", "Switzerland", "United Kingdom"]
+subprocess.call(["C:/Program Files (x86)/NordVPN/NordVPN.exe", "-c", "-g", countries[random.randrange(15)]])
