d577bb2e7bb0676177017ba76e188f65185f553d
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-04-20 17:36:15 +0200

    More

--- Webscraping with BeautifulSoup/ariva_HistoricalPrices.py
+++ Webscraping with BeautifulSoup/ariva_HistoricalPrices.py
@@ -12,39 +12,41 @@ import random
 import subprocess
 import json
 
-
-# DAX-Unternehmen einlesen
-# DAX30 Unternehmen + Ex-Unternehmen
-def dax_stocks ():
-    page = requests.get ("https://www.ariva.de/dax-30")
+# Unternehmen eines bestimmten Index werden eingelesen
+# Output: Dict in der Form Kürzel von Ariva.de + Name des Titels (z.b '/apple-aktie': 'Apple')
+def read_index(index_name):
+    page = requests.get ("https://www.ariva.de/"+index_name)
     soup = BeautifulSoup (page.content, "html.parser")
     table  = soup.find(id="result_table_0")
-    dax = {}
+    index_stocks = {}
     for row  in table.find_all("td"):
         if row.get("class") == ["ellipsis", "nobr", "new", "padding-right-5"]:
-            dax[row.find("a")["href"]] = row.text.strip()
-            #print(row.find("a")["href"])
-            #print(row.get("class"))
-            #print(row)
-    print(dax)
+            index_stocks[row.find("a")["href"]] = row.text.strip()
+    index_stocks = sorted(index_stocks.items (), key=lambda x: x[1])
+    return(index_stocks)
 
-
-def vpn_switch():
+# VPN-Switch bei NordVPN mit x Sekunden Verzögerung
+# Output: Rückgabe des zufällig gewählten Landes
+def vpn_switch(sek):
     countries = ["Argentina", "Australia", "Austria", "Belgium", "Canada", "Germany", "Israel", "Italy",
                  "Norway", "Poland", "Portugal", "Romania", "Serbia", "Switzerland", "United Kingdom"]
     rand_country = random.randrange(len(countries)-1)
     subprocess.call (["C:/Program Files (x86)/NordVPN/NordVPN.exe", "-c", "-g", countries[rand_country]])
-    time.sleep(30)  #Verzögerung von x Sekunden
+    time.sleep(sek)  #Verzögerung von x Sekunden
     print ("Connected to ", countries[rand_country],"...")
-
+    return(countries[rand_country])
 
 # Aktienkurse für eine Unternehmen einlesen
 # boerse_id ist fix =>  6 = Xetra
 # Input Stock: Aktienkennung lt. Ariva.de z.b. /apple-aktie oder /wirecard-aktie
 # Input Month: Monatsultimo im Format z.B. 2019-04-30
+# Output: bei Spaltenposition 0: Ausgabe/Yield von Schlüsselwort "datum" + Inhalt des Datumsfeldes im Format dd.mm.yyyy
+# Output: bei Spaltenposition 4: Ausgabe/Yield von Schlüsselwort "price" + Inhalt des Schlusskurses als Zahl
+# Output: wenn IP-Seitensperre erfolgt ist (Kein Zugriff auf HTML-Seite im Text): Ausgabe/Yield von Schlüsselwort "abbruch" + Textinhalt
 def stock_prices (stock,month):
     # read table with monatlichen Kursen
-    url = "https://www.ariva.de" + stock + "/historische_kurse?boerse_id=6&month=" + month + "&currency=EUR&clean_split=1&clean_split=0&clean_payout=0&clean_bezug=1&clean_bezug=0"
+    url = "https://www.ariva.de" + stock + "/historische_kurse?boerse_id=6&month=" + month + \
+          "&currency=EUR&clean_split=1&clean_split=0&clean_payout=0&clean_bezug=1&clean_bezug=0"
     page = requests.get (url)
     soup = BeautifulSoup (page.content, "html.parser")
     # Check ob Zugriff auf die IP-Adresse gesperrt ist
@@ -66,7 +68,6 @@ def stock_prices (stock,month):
             elif col_id == 4:   # 5.Spalte Schlusskurs
                 yield "price", col_content.text.strip()
 
-
 # Monatsultimo ermitteln für Zeitraum
 # Input z.B. (3, 2015, datetime.now().month, datetime.now().year) bei Aufruf
 # Output z.b. Ultimo-Datum z.b. 2016-04-30
@@ -79,14 +80,12 @@ def month_year_iter( start_month, start_year, end_month, end_year ):
         #print(datetime.date(y,m+1,calendar.monthrange(y,m+1)[1]))
         yield datetime.date(y,m+1,calendar.monthrange(y,m+1)[1])
 
-
 # Erstellung einer Datums-Liste vom aktuellstem bis zum  ältesten Datum im Format jjjj-mm-dd
 def date_list (datum_von, datum_bis):
     mydates2  = []
     mydates = pd.date_range(datum_bis, datum_von).tolist()
     for i in range(len(mydates)-1,-1,-1): mydates2.append(mydates[i].strftime('%d.%m.%y'))
     return(mydates2)
-
 
 # Ausgabe der Liste als CSV-File
 def csv_write(result, filename):
@@ -101,6 +100,42 @@ def csv_write(result, filename):
             input()
     fp.close()
 
+# Spalten bereinigen der Tabelle
+# Datum vereinheitlichen + leere Spalten löschen
+def spalten_bereinigen(output):
+    # Datum-Spalten vereinheitlichen
+    for i in range (2, len (output[0]) - 1):
+        for j in range (1, len (output) - 1):
+            if j % 2 == 1:
+                if i == len (output[j]):
+                    output[j].insert (i, "")
+                    output[j + 1].insert (i, "")
+                elif output[j][i] != output[0][i]:
+                    output[j].insert (i, "")
+                    output[j + 1].insert (i, "")
+            else:
+                continue
+    # Check ob die letzte Spalte bei den Aktientiteln leer ist - sonst fehlt eine Spalte am Ende bei den Aktientiteln
+    for i in range (1, len (output) - 1):
+        if i % 2 == 1 and len (output[0]) != len (output[i]):
+            output[i].insert (len (output[i]), "")
+            output[i + 1].insert (len (output[i + 1]), "")
+
+    # Leere Spalten löschen
+    # kein Kurse für eine Aktie an diesem Tag
+    pos_del = []
+    for i in range (2, len (output[0])):
+        empty = True
+        for j in range (1, len (output) - 1):
+            if output[j][i] != "":
+                empty = False
+                break
+        if empty == True: pos_del.append (i)
+    pos_del.reverse ()
+    for k in pos_del:
+        for m in range (len (output)):
+            del output[m][k]
+    return(output)
 
 # Einlesen eines Ergebnis-XLS um die Datümer zusätzlich abzugleichen
 # Durchgängiges Datum muss noch vorne ergänzt werden
@@ -124,6 +159,7 @@ def read_XLS():
 
 
 # DAX30 Unternehmen + Ex-Unternehmen
+# Input: Für welche Aktientitel die Verarbeitung erfolgen soll
 """
 stocks_dic = {'/apple-aktie': 'Apple', '/infineon-aktie': 'Infineon', '/volkswagen_vz-aktie': 'Volkswagen Vz', '/continental-aktie': 'Continental',
 '/bmw-aktie': 'BMW St', '/heidelbergcement-aktie': 'HeidelbergCement', '/mtu_aero_engines-aktie': 'MTU Aero Engines', '/covestro-aktie': 'Covestro',
@@ -137,41 +173,28 @@ stocks_dic = {'/apple-aktie': 'Apple', '/infineon-aktie': 'Infineon', '/volkswag
  , '/hannover_rück-aktie': 'Hannover Rück', '/tui-aktie': 'TUI', '/mlp-aktie': 'MLP'}
 """
 
-#stocks_dic = {'/apple-aktie': 'Apple', '/infineon-aktie': 'Infineon'}
-stocks_dic = {'/bmw-aktie': 'BMW St', '/heidelbergcement-aktie': 'HeidelbergCement', '/mtu_aero_engines-aktie': 'MTU Aero Engines', '/covestro-aktie': 'Covestro',
-'/siemens-aktie': 'Siemens', '/daimler-aktie': 'Daimler', '/munich_re-aktie': 'Munich Re', '/basf-aktie': 'BASF', '/deutsche_bank-aktie': 'Dt. Bank',
-'/deutsche_post-aktie': 'Dt. Post', '/adidas-aktie': 'adidas', '/allianz-aktie': 'Allianz', '/sap-aktie': 'SAP', '/deutsche_telekom-aktie': 'Dt. Telekom',
-'/linde_plc-aktie': 'Linde PLC', '/bayer-aktie': 'Bayer', '/henkel_vz-aktie': 'Henkel Vz', '/lufthansa-aktie': 'Lufthansa', '/beiersdorf-aktie': 'Beiersdorf',
-'/merck_kgaa-aktie': 'Merck KGaA', '/fresenius_medical_care-aktie': 'Fresenius Medical Care', '/deutsche_b%C3%B6rse-aktie': 'Dt. Börse', '/fresenius-aktie': 'Fresenius',
-'/wirecard-aktie': 'Wirecard', '/rwe-aktie': 'RWE St', '/e-on-aktie': 'E.ON', '/vonovia-aktie': 'Vonovia', '/thyssenkrupp-akti': 'ThyssenKrupp',
-'/commerzbank-aktie': 'Commerzbank', '/prosiebensat-1_media-aktie': 'ProSiebenSat-1 Media', '/uniper-aktie': 'Uniper', '/k-s-6-aktie': 'KS6',
- '/lanxess-aktie': 'Lanxess', '/osram_licht-aktie': 'Osram Licht', '/ceconomy_st-aktie': 'Ceconomy St', '/man-aktie': 'MAN', '/salzgitter-aktie': 'Salzgitter'
- , '/hannover_rück-aktie': 'Hannover Rück', '/tui-aktie': 'TUI', '/mlp-aktie': 'MLP'}
+stocks_dic = {'/thyssenkrupp-aktie': 'ThyssenKrupp'}
 
-#stocks = ["/prosiebensat-1_media-aktie","/linde_plc-aktie"]
-#stocks = ["/apple-aktie","/wirecard-aktie", "/rwe-aktie", "/heidelbergcement-aktie","/volkswagen_vz-aktie"]
-#stocks = ["/apple-aktie"]
-
-#stocks = ["/volkswagen_vz-aktie", "/fresenius-aktie", "/sap-aktie", "/bayer-aktie",
-# "/deutsche_b%C3%B6rse-aktie", "/merck_kgaa-aktie", "/fresenius_medical_care-aktie", "/linde_plc-aktie",
-# "/allianz-aktie", "/deutsche_post-aktie", "/covestro-aktie", "/henkel_vz-aktie", "/siemens-aktie",
-# "/beiersdorf-aktie", "/continental-aktie", "/deutsche_telekom-aktie", "/bmw-aktie", "/vonovia-aktie",
-# "/deutsche_bank-aktie", "/daimler-aktie", "/basf-aktie", "/adidas-aktie", "/munich_re-aktie",
-# "/lufthansa-aktie", "/infineon-aktie", "/e-on-aktie", "/mtu_aero_engines-aktie",
-# "/thyssenkrupp-aktie","/commerzbank-aktie","/prosiebensat-1_media-aktie","/linde_plc-aktie","/uniper-aktie",
-# "/k-s-6-aktie","/lanxess-aktie","/osram_licht-aktie","/ceconomy_st-aktie","/man-aktie","/salzgitter-aktie",
-# "/hannover_rück-aktie","/infineon-aktie","/tui-aktie","/lanxess-aktie","/mlp-aktie","/daimler-aktie"]
-#
-
-start_gesamt = timeit.default_timer()
+#Input-Parameter
+#Input - Angabe welcher Index gelesen werden soll (z.B. DAX-30) - bei Angabe von 0 wird individuell lt. stocks_dic eingelesen
+#Input - start_year, start_month: wie weit in die Historie zurückgegangen wird (z.b. bis 1995 06)
+#Input - end_year, end_month: von welchem Datum die Ermittlung weg erfolgt - wenn year = 0 wird aktuelles Tagesdatum genommen
+#Input - sek: Anzahl der Sekunden der Verzögerung bei VPN-Switch
+index = 0
 start_year = 1989
 start_month = 1
 end_year = 0
 end_month = 0
+sek = 30
+
+start_gesamt = timeit.default_timer()
+if index != 0:
+    stocks_dic = read_index(index)
 if end_year == 0:
     end_year = datetime.datetime.now().year
     end_month = datetime.datetime.now().month
 output = []
+vpn_performance = []
 #Datumszeile erstellen für den gesamten Zeitraum pro Tag
 datelist = ["Kürzel","Datum"]
 datelist.extend(date_list(datetime.date.today(), datetime.date(start_year, start_month,1)))
@@ -200,8 +223,8 @@ for stock in stocks_dic:
             break
     if skip == True:
         continue
-    #switch der vpn-verbindung
-    vpn_switch()
+    #switch der vpn-verbindung mit sek-Verzögerung
+    vpn_land = vpn_switch(sek)
     start_stock = timeit.default_timer()
     title_row = [stock,stocks_dic.get(stock)]
     stock_row = [stock,stocks_dic.get(stock)]
@@ -227,65 +250,35 @@ for stock in stocks_dic:
     output.append(stock_row)
 
     # Ausgabe der Zeilen in Temp-Textfile...
-    print(title_row)
-    print(stock_row)
     with open('tempdata.txt', 'a') as file:
         file.write("%s\n" %title_row)
         file.write ("%s\n" %stock_row)
 
     stop_stock = timeit.default_timer ()
-    print("Laufzeit Aktie ",stock," : ",round((stop_stock-start_stock)/60,2),"min")
+    laufzeit = round((stop_stock-start_stock)/60,2)
+    print("Laufzeit Aktie ",stock," : ",laufzeit,"min mit Server aus ", vpn_land)
+    vpn_performance.append("Laufzeit Aktie "+stock+" : "+str(laufzeit)+"min mit Server aus "+vpn_land)
+
+
 stop_readstocks = timeit.default_timer()
 
-# csv_write([list(filter(None,i)) for i in zip_longest(*output)], "prices_dax_unordered.csv")
+#Transponieren der Tabelle + Ausgabe als Zwischentabelle in CSV
 csv_write(list(zip_longest(*output)), "prices_dax_unordered.csv")
 
 start_spaltenaufbereitung = timeit.default_timer()
-print("Spalten bereinigen...")
-# Datum-Spalten vereinheitlichen
-for i in range(2, len(output[0])-1):
-    for j in range(1,len(output)-1):
-        if j%2 == 1:
-            if i == len(output[j]):
-                output[j].insert (i, "")
-                output[j + 1].insert (i, "")
-            elif output[j][i] != output[0][i]:
-                output[j].insert(i,"")
-                output[j+1].insert (i, "")
-        else: continue
-# Check ob die letzte Spalte bei den Aktientiteln leer ist - sonst fehlt eine Spalte am Ende bei den Aktientiteln
-for i in range(1,len(output)-1):
-    if i%2 == 1 and len(output[0]) != len(output[i]):
-        output[i].insert (len(output[i]), "")
-        output[i+1].insert (len(output[i+1]), "")
-
-# Leere Spalten löschen
-# kein Kurse für eine Aktie an diesem Tag
-pos_del = []
-for i in range(2, len(output[0])):
-    empty=True
-    for j in range (1, len (output) - 1):
-        if output[j][i] != "":
-            empty=False
-            break
-    if empty == True: pos_del.append(i)
-pos_del.reverse()
-for k in pos_del:
-    for m in range (len(output)):
-        del output[m][k]
-
+print ("Spalten bereinigen...")
+output = spalten_bereinigen(output)
 print("Transponieren und Ausgeben...")
 # Transponieren der Tabelle und Ausgabe als CSV-File
-# result = output       #Output mit Datümern auf Spaltenebene - funktioniert nur bis zu einer gewissen Größe...
-
-#result = [list(filter(None,i)) for i in zip_longest(*output)]
 result = list(zip_longest(*output))
-
+# Aussgabe der finalen Ergebnistabelle als CSV
 csv_write(result, "prices_dax_withdates.csv")
-
 stop_spaltenaufbereitung = timeit.default_timer()
+
 stop_gesamt = timeit.default_timer()
 print("Gesamtlaufzeit: ", round((stop_gesamt-start_gesamt)/60,2), "min")
 print("Aktienkurse Gesamt: ", round((stop_readstocks-start_readstocks)/60,2), "min")
 print("Spaltenaufbereiung: ", round((stop_spaltenaufbereitung-start_spaltenaufbereitung)/60,2), "min")
+print("Statistik VPNs:")
+for i in vpn_performance: print(i)
 
--- Webscraping with BeautifulSoup/temp6.py
+++ Webscraping with BeautifulSoup/temp6.py
@@ -1,3 +1,16 @@
-txt = "GuV/Bilanz in Mio. USD nach US GAAP - Geschäftsjahresende: 30.09."
-new = "Bilanz in Mio. " + txt[18:22].strip() + " per " + txt[-7:-1].strip()
-print(new)
+import requests
+from bs4 import BeautifulSoup
+
+def read_index(index_name):
+    page = requests.get ("https://www.ariva.de/"+index_name)
+    soup = BeautifulSoup (page.content, "html.parser")
+    table  = soup.find(id="result_table_0")
+    index_stocks = {}
+    for row  in table.find_all("td"):
+        if row.get("class") == ["ellipsis", "nobr", "new", "padding-right-5"]:
+            index_stocks[row.find("a")["href"]] = row.text.strip()
+    index_stocks = sorted(index_stocks.items (), key=lambda x: x[1])
+    return(index_stocks)
+
+idx = read_index("dax-30")
+print(idx)
