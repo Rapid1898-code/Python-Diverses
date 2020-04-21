5a1f5fdb2142f03bb9fd70f4ba85f0189a5c29c2
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-04-21 08:14:49 +0200

    More

--- Webscraping with BeautifulSoup/ariva_Bilanz_GuV.py
+++ Webscraping with BeautifulSoup/ariva_Bilanz_GuV.py
@@ -22,18 +22,6 @@ def read_bilanz(stock):
     seite = 0
     jahre_titelleiste = []
 
-    # output_gesamt initial definieren
-    # output bilden für seite (+bereinigung)
-    # SPRUNG1: aktuelle titelzeitleiste speichern
-    # seite um 6 erhöhen
-    # output bilden für (neue) seite (+bereinigung)
-    # wenn titelzeitleiste = gespeicherte titelzeitleiste => Break! aus while True Schleife und return des ergebnises
-    # ermitteln welche Spalten eingefügt werden müssen
-        #von rechts nach links lesen
-        #wenn Jahr schon vorhanden => skip
-        #wenn Jahr noch nicht vorhanden => an Pos1 in output_gesamt einfügen für jede Zeile (evt. inkl. Überprüfung ob Text zusammenpasst)
-    # zu SPRUNG1
-
     while True:
         link = "https://www.ariva.de" + stock + "/bilanz-guv?page=" + str(seite) + "#stammdaten"
         page = requests.get (link)
@@ -52,17 +40,17 @@ def read_bilanz(stock):
         for i in range(len(output_temp)-1,0,-1):
             #Quartalswerte entfernen
             if "Quartal" in output_temp[i][0]: del output_temp[i]
-            #Leerzeilen entfernen
+            #Leerzeilen entfernen + Überschriften Aktive/Passiva
             empty = True
-            for j in range(len(output_temp[0])-1):
+            for j in range(1, len(output_temp[0])-1):
                 if output_temp[i][j] != "": empty = False
             if empty == True: del output_temp[i]
             #Doppelte Jahreszahlen entfernen
             if i != 0 and output_temp[i] == output_temp[0]: del output_temp[i]
+            #Formate und Texte anpassen
             text_dic = {'Umsatz': 'Revenue in M', 'Bruttoergebnis vom Umsatz': 'Gross Profit in M ', 'Operatives Ergebnis (EBIT)': 'EBIT in M',
                         'Finanzergebnis':'Financial Result in M','key':'val','key':'val','key':'val','key':'val','key':'val','key':'val','key':'val','key':'val',
                         }
-            #Formate und Texte anpassen
 
         #Eckdaten Kennzahlen ermitteln und links oben im Sheet speichern
         if seite == 0:
@@ -72,10 +60,6 @@ def read_bilanz(stock):
                     output_temp[0][0] = "Bilanz in Mio. " + i.text.strip()[18:22] + " per " + i.text.strip()[-7:-1]
 
         #Einfügen weiterer Jahreswerte
-
-        #print (output_temp[0][1:7])
-        #print (jahre_titelleiste)
-
         if output_temp[0][1:7] == jahre_titelleiste:
             break
         else:
@@ -84,29 +68,32 @@ def read_bilanz(stock):
             if output_gesamt == []:
                 output_gesamt = output_temp
             else:
-
-                print ("drinne!", len(output_temp[0]))
-
                 for i in range(len(output_temp[0])-1,1,-1):
-
-                    print(i)
-                    #print(output_temp[0][i])
-                    #print(output_gesamt[0][1:len(output_gesamt[0])-1])
-
                     if output_temp[0][i] in output_gesamt[0][1:len(output_gesamt[0])-1]: continue
                     else:
                         pos_insert = i
                         break
-                for i in range(pos_insert,1,-1):
-                    for j in range (0,len(output_gesamt)-1):
-                        output_gesamt[j][1] = output_temp[j][i]
-                        if j > 0 and output_gesamt[j] != output_temp[j]:
-                            print("Beschriftung unterschiedlich in Output-Gesamt und Output-Temp in Zeile: ",j)
+                for i in range(pos_insert,0,-1):
+                    for j in range (len(output_gesamt)):
+                        output_gesamt[j].insert(1, output_temp[j][i])
+                        if j > 0 and output_gesamt[j][0] != output_temp[j][0]:
+                            print("FEHLER !!! Beschriftung unterschiedlich in Output-Gesamt und Output-Temp in Zeile: ",j)
             output_temp = []
-    return output
 
+    #Leerspalten bereinigen (wo nur "-" enthalten ist
+    for i in range (len(output_gesamt[0])-1,0,-1):
+        empty=True
+        for j in (1, len (output_gesamt)-1):
+            if output_gesamt[j][i] not in ["-",""]:
+                empty=False
+                break
+        if empty==True:
+            for j in range (len(output_gesamt)):
+                del output_gesamt[j][i]
 
-stocks_dic = {'/apple-aktie': 'Apple'}
+    return output_gesamt
+
+stocks_dic = {'/sap-aktie': 'Apple'}
 for stock in stocks_dic:
     output = read_bilanz(stock)
 csv_write(output,"ariva_data.csv")
