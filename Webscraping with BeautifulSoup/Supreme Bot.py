f84b2c9cd672889628cee4e8ee5be7b2132b6d85
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-05-29 22:23:15 +0200

    More

--- Webscraping with BeautifulSoup/Supreme Bot.py
+++ Webscraping with BeautifulSoup/Supreme Bot.py
@@ -1,9 +1,23 @@
 from config import keys
+import os
 from selenium import webdriver
-
+import time
 
 def order(k):
-
+    driver = webdriver.Chrome(os.getcwd() + '/chromedriver')
+    driver.get(k["product_url"])
+    driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="cart"]/a[2]').click()
+    driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(k["name"])
+    driver.find_element_by_xpath ('')
+    driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(k["phone_number"])
+    driver.find_element_by_xpath('//*[@id="bo"]').send_keys(k["address"])
+    driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(k["zip"])
+    #driver.find_element_by_xpath('')
+    #driver.find_element_by_xpath('')
+    #driver.find_element_by_xpath('')
+    #driver.find_element_by_xpath('')
 
 if __name__ == "__main__":
-    order()
+    order(keys)
--- Webscraping with BeautifulSoup/config.py
+++ Webscraping with BeautifulSoup/config.py
@@ -1,2 +1,6 @@
 keys = {"product_url": "https://www.supremenewyork.com/shop/jackets/qxgy36ias",
-        "name": "John"}
+        "name": "John",
+        "email": "testmail@gmx.com",
+        "address": "Forest Hill Drive 3",
+        "zip": "1230"
+        }
