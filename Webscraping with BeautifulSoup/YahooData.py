aee8f046e4153ee7a3f9a7397cd4919343e9005e
Rapid1898 <markuspolzer@gmx.at>
2020-06-09 09:53:20 +0200

    Update

--- Webscraping with BeautifulSoup/YahooData.py
+++ Webscraping with BeautifulSoup/YahooData.py
@@ -8,29 +8,83 @@ from bs4 import BeautifulSoup
 # https://www.gurufocus.com/term/ev/BAYZF/Enterprise-Value/Bayer-AG
 # https://www.gurufocus.com/term/ev/AAPL/Enterprise-Value/Apple
 
-link = "https://finance.yahoo.com/quote/AAPL?p=AAPL"
+symbol = "AAPL"
+link = "https://finance.yahoo.com/quote/"+symbol
+link2 = link+"/profile?p="+symbol
 
 page = requests.get (link)
 soup = BeautifulSoup (page.content, "html.parser")
 
-# Read actual price
+# Read stockname, actual price, daychange
 table = soup.find('div', id="quote-header-info")
-erg = table.find('div', attrs={"class": ""})
-for idx,cont in enumerate(erg):
-    if idx==0: print(cont.text)
+name = table.find("h1").text.split("-")[1].strip()
+price = soup.find('span', attrs={"data-reactid": "14"}).text.strip()
+daychange_tmp = soup.find('span', attrs={"data-reactid": "16"}).text.strip().split("(")
+daychange = daychange_tmp[0].strip()
+daychange_perc = daychange_tmp[1].strip().replace(")","")
 
-# Read Stock Name
-name = table.find("h1")
-print (name.text.strip())
-
+# Read Volumes
 table = soup.find('div', id="quote-header")
-volume = soup.find('td', attrs={"data-test": "TD_VOLUME-value"})
-print(volume.text.strip())
-avg_volume = soup.find('td', attrs={"data-test": "AVERAGE_VOLUME_3MONTH-value"})
-print(avg_volume.text.strip())
+volume = soup.find('td', attrs={"data-test": "TD_VOLUME-value"}).text.strip()
+avg_volume = soup.find('td', attrs={"data-test": "AVERAGE_VOLUME_3MONTH-value"}).text.strip()
 
-for row in table.find_all("td"):
-    print(row)
+# Read Ranges
+d_r_tmp = soup.find('td', attrs={"data-test": "DAYS_RANGE-value"}).text.strip().split('-')
+day_range_from, day_range_to = d_r_tmp[0].strip(), d_r_tmp[1].strip()
+f_r_temp = soup.find('td', attrs={"data-test": "FIFTY_TWO_WK_RANGE-value"}).text.strip().split('-')
+fifty_range_from, fifty_range_to = f_r_temp[0].strip(), f_r_temp[1].strip()
+
+# Read MarketCap, PrevClose, PE, EPS
+marketcap = soup.find('td', attrs={"data-test": "MARKET_CAP-value"}).text.strip()
+prevclose = soup.find('td', attrs={"data-test": "PREV_CLOSE-value"}).text.strip()
+pe_ratio = soup.find('td', attrs={"data-test": "PE_RATIO-value"}).text.strip()
+eps_ratio = soup.find('td', attrs={"data-test": "EPS_RATIO-value"}).text.strip()
+print(prevclose)
+
+for row in soup.find_all('td'): print(row)
+
+# Read Additional Infos
+page = requests.get (link2)
+soup = BeautifulSoup (page.content, "html.parser")
+empl = soup.find('span', attrs={"data-reactid": "30"}).text.strip()
+sector_tmp = soup.find_all('span', attrs={"data-reactid": "21"})
+for row in sector_tmp:
+    if row.get("class") != None: sector = row.text.strip()
+industry_tmp = soup.find_all('span', attrs={"data-reactid": "25"})
+for row in industry_tmp:
+    if row.get("class") != None: technology = row.text.strip()
+description = soup.find('p', attrs={"data-reactid": "141"}).text.strip()
+print(empl)
+print(technology)
+print(description)
+
+print("\nALERT TRACKER")
+print("Symbol:", symbol)
+print("Name:", name)
+print("Sector:", sector)
+print("Price:", price)
+print("DayChange:",daychange)
+print("DayChange_Perc:",daychange_perc)
+print("MarketCap:", marketcap)
+print("PE-Rato:", pe_ratio)
+print("EPS-Ratio:", eps_ratio)
+print("52W Low:", fifty_range_from)
+print("52W HIgh:", fifty_range_to)
+print("\nDASHBOARD")
+
+print("Current Volume:", volume)
+print("Average Volume:", avg_volume)
+print("Day Low/High:", day_range_from,"/", day_range_to)
+print("52W Range:", fifty_range_from,"/",fifty_range_to)
+
+
+
+
+
+
+
+
+
 
 
 
