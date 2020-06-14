e0fa54bd2cc598844402426658da716359d32152
Rapid1898 <markuspolzer@gmx.at>
2020-06-14 09:56:49 +0200

    Update

--- Webscraping with BeautifulSoup/temp.py
+++ Webscraping with BeautifulSoup/temp.py
@@ -1,65 +1,61 @@
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
-from openpyxl.styles import NamedStyle, Font, PatternFill, Border, Side
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import os
+import time
 
-name_xlsx = "Test.xlsx"
-wb =  load_workbook(name_xlsx)
-wb._sheets.sort(key=lambda x: x.title)
-if "INDEX" in wb.sheetnames: del wb["INDEX"]
-wb.create_sheet("INDEX",0)
-ws_idx = wb["INDEX"]
-link_idx =  name_xlsx + "#" + "INDEX" + "!A1"
-ws_idx["A1"] = "INDEX"
+SCROLL_PAUSE_TIME = 0.5
+COUNT_SCROLL_DOWN = 5
+EMOJI = "🚨"     #🤝‍♂💪🚨
+STOCKWITS = "mrinvestorpro"
 
-column_width = 0
-for i, ws in enumerate(wb):
-    if ws.title == "INDEX": continue
-    if len(ws.title) > column_width: column_width = len(ws.title)
-    link = name_xlsx + "#" + ws.title + "!A1"
-    ws_idx.cell (row=i+2, column=1).value = '=HYPERLINK("{}", "{}")'.format (link, ws.title)
-    wb[ws.title].cell (row=1, column=6).value = '=HYPERLINK("{}", "{}")'.format (link_idx, "Back to INDEX")
 
-#Formatierung
-bold = Font (bold=True)
-bg_yellow = PatternFill (fill_type="solid", start_color='fbfce1', end_color='fbfce1')
-bg_grey = PatternFill (fill_type="solid", start_color='babab6', end_color='babab6')
-bg_green = PatternFill (fill_type="solid", start_color='c7ffcd', end_color='fffbc7')
-frame_all = Border (left=Side (style='thin'), right=Side (style='thin'), top=Side (style='thin'),
-                    bottom=Side (style='thin'))
-frame_upanddown = Border (top=Side (style='thin'), bottom=Side (style='thin'))
-size14 = Font (bold=True, size="14")
+link = "https://stocktwits.com/" + STOCKWITS
+driver = webdriver.Chrome(os.getcwd() + '/chromedriver')
+driver.get(link)
+time.sleep(2)
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
 
-for cell in ws_idx["A:A"]:
-    cell.font = bold
-    cell.fill = bg_yellow
-    cell.border = frame_all
-for cell in ws_idx["2:2"]:
-    cell.fill = bg_grey
-    cell.border = frame_upanddown
-for cell in ws_idx["1:1"]:
-    cell.font = bold
-    cell.fill = bg_green
-    cell.border = frame_all
-ws_idx["A1"].font = size14
-freeze = ws_idx["C2"]
-ws_idx.freeze_panes = freeze
-ws_idx.column_dimensions["A"].width = column_width
-
-while True:
-    try:
-        wb.save (name_xlsx)
-        wb.close ()
+for i in range(COUNT_SCROLL_DOWN):
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
         break
-    except Exception as e:
-        print ("Error: ", e)
-        input ("Datei kann nicht geöffnet werden - bitte schließen und <Enter> drücken!")
+    last_height = new_height
 
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+table = soup.find ('div', attrs={"infinite-scroll-component__outerdiv"})
+divs = table.find_all ("div")
+erg_divs=[]
+erg_stocks=[]
+for i in divs:
+    if EMOJI in i.text and i.text.strip() not in erg_divs:
+        if erg_divs != []:
+            for j in i.text.strip().split():
+                if "$" in j and j.replace("$","") not in erg_stocks: erg_stocks.append(j.replace("$",""))
+                break
+        erg_divs.append(i.text.strip())
+if erg_divs != []: erg_divs.pop(0)
+for i in erg_divs: print(i,"\n")
+print(erg_stocks)
+time.sleep (2)
+driver.quit ()
 
-
-
-
-
-
-
+"""
+table = soup.find ('div', attrs={"class": "st_gIQ1cL6 st_2-AYUR9"})
+spans = table.find_all ("span")
+sector = spans[1].text.strip()
+industry = spans[3].text.strip()
+empl = spans[5].text.strip()
+table = soup.find ('section', attrs={"class": "quote-sub-section Mt(30px)"})
+description = table.find("p").text.strip()
+print(sector)
+print(industry)
+print(description)
+"""
 
---
+++ Webscraping with BeautifulSoup/temp2.py
@@ -0,0 +1,2 @@
+s = "das ist ein txt"
+print(s.split()[0])
