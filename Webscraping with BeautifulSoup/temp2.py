36cea004a6c71bbc4b8d479aff523e951cf886d8
Rapid1898 <markuspolzer@gmx.at>
2020-06-14 21:58:51 +0200

    Update

--- Webscraping with BeautifulSoup/temp2.py
+++ Webscraping with BeautifulSoup/temp2.py
@@ -1,2 +1,10 @@
-s = "das ist ein txt"
-print(s.split()[0])
+from datetime import datetime
+now = datetime.now()
+d = datetime(2020,3,5,19,27,23)
+print(now)
+print(d)
+diff = now-d
+print(diff.days)
+print(diff.seconds)
+print(diff.total_seconds()/60)
+
