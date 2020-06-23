de30c619783c84404edb28c0882a50ef044c0e16
Rapid1898 <markuspolzer@gmx.at>
2020-06-23 23:29:30 +0200

    Update

--- PyGame/hangman.py
+++ PyGame/hangman.py
@@ -10,10 +10,14 @@ pg.display.set_caption("Hangman Game")
 # load images
 images = []
 for i in range(6):
-    pg.image.load("hangman0.png")
-    # image = pg.image.load("hangman" + str(i) + ".png")
-    images.append()
-print(images)
+    image = pg.image.load("hangman" + str(i) + ".png")
+    images.append(image)
+
+# game variables
+hangman_status = 0
+
+# colors
+WHITE = (255,255,255)
 
 # setup game loop
 FPS = 60
@@ -22,6 +26,11 @@ run = True
 
 while run:
     clock.tick(FPS)
+
+    win.fill(WHITE)
+    win.blit(images[hangman_status], (150,100))
+    pg.display.update()
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
