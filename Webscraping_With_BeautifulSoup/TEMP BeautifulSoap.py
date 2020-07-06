8cdb59fca42e04c8882d8160e52dd533ed5bea99
Rapid1898 <markuspolzer@gmx.at>
2020-07-06 22:44:22 +0200

    Update

--- Webscraping_With_BeautifulSoup/TEMP BeautifulSoap.py
+++ Webscraping_With_BeautifulSoup/TEMP BeautifulSoap.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-link1 = "http://www.aastocks.com/en/stocks/market/index/hk-index-con.aspx"
+link1 = "https://finance.yahoo.com/quote/AAPL/key-statistics?p=AAPL"
 page1 = requests.get (link1)
 soup1 = BeautifulSoup (page1.content, "html.parser")
+table  = soup1.find(id="Col1-0-KeyStatistics-Proxy").get_attribute("innerHTML")
 
+
+
+"""
 erg_stock = {}
 list = []
 
@@ -14,7 +18,7 @@ for e in soup1.find_all("a"):
 
 erg_stock["hang-seng"] = list
 for key, val in erg_stock.items (): print (key, val)
-
+"""
 
 
 
