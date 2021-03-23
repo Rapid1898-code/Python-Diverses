71768660b71d15b8c115daa7be7c97730d268785
Rapid1898 <markuspolzer@gmx.at>
2021-03-23 20:18:40 +0100

    upd

---
+++ Kivy/PlusKivyMD/41TeachYourself.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+Builder.load_file("41TeachYourself.kv")
+
+class MyLayout(Widget):
+    pass
+
+class AwesomeApp(MDApp):
+    def build(self):
+        return MyLayout()
+
+if __name__ == "__main__":
+    AwesomeApp().run()
