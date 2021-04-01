198ac9f6efa70ed90fe5cff1dfcac1bbd1cc46f4
Rapid1898 <markuspolzer@gmx.at>
2021-04-01 21:46:19 +0200

    upd

--- Kivy/PlusKivyMD/LevermannApp2.py
+++ Kivy/PlusKivyMD/LevermannApp2.py
@@ -1,10 +1,12 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.core.window import Window
 
 class MainApp(MDApp):
+    Window.size = (550, 700)    
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_palette = "Red"
         return Builder.load_file("LevermannApp2.kv")
 
     # def logger(self):
