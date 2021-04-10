a22f285e148943b51a1e137362c9d05cbf4c1c51
Rapid1898 <markuspolzer@gmx.at>
2021-04-10 13:28:01 +0200

    upd

--- YFinance/yFinanceTest.py
+++ YFinance/yFinanceTest.py
@@ -3,15 +3,17 @@ from datetime import datetime, timedelta
 from datetime import date
 
 ticker = "AAPL"
+ticker = "CAT"
+# ticker = "1010.SR"
 # ticker = "BTC-USD"
 
 dataYF = yf.Ticker(ticker)
 
 
-# # Summary Infos
-# for key, val in dataYF.info.items ():
-#     if val not in [False,None]:
-#         print (f"{key} => {val} {type(val)}")
+# Summary Infos
+for key, val in dataYF.info.items ():
+    if val not in [False,None]:
+        print (f"{key} => {val} {type(val)}")
 
 # # Price data
 # tday = datetime.today()
@@ -20,19 +22,20 @@ dataYF = yf.Ticker(ticker)
 # # Intraday Price data
 # print(dataYF.history(period="1mo",interval="2m"))
 
-print(dataYF.financials)
-print(dataYF.dividends)
-print(dataYF.splits)
-print(dataYF.major_holders)
-print(dataYF.institutional_holders)
+# print(dataYF.financials.loc["Net Income",].iloc[0])
 
-print(dataYF.balance_sheet)
-print(dataYF.cashflow)
-print(dataYF.earnings)
-print(dataYF.sustainability)
-print(dataYF.recommendations)
-print(dataYF.calendar)
-print(dataYF.isin)
+# print(dataYF.dividends)
+# print(dataYF.splits)
+# print(dataYF.major_holders)
+# print(dataYF.institutional_holders)
+
+# print(dataYF.balance_sheet)
+# print(dataYF.cashflow)
+# print(dataYF.earnings)
+# print(dataYF.sustainability)
+# print(dataYF.recommendations)
+# print(dataYF.calendar)
+# print(dataYF.isin)
 
 
 
